3aa93a53a50782069ff92680588d6e74d02d79e5
Jesus <jesusdan@unitec.edu>
2023-06-04 00:02:25 -0600

    Se termino el documento Joins_movies.sql

--- Tarea3/Joins/Joins_movies.sql
+++ Tarea3/Joins/Joins_movies.sql
@@ -65,3 +65,55 @@ inner join movie m
 ON m.movie_id = mc.movie_id
 
 -- 7 movies --
+SELECT
+	pc.company_name,
+	c.country_name
+FROM movie m
+inner join production_country pc2
+ON pc2.movie_id = m.movie_id 
+inner join country c 
+ON c.country_id = pc2.country_id 
+inner join movie_company mc 
+ON mc.movie_id = m.movie_id
+inner join production_company pc 
+ON  pc.company_id = mc.company_id
+order by c.country_name 
+
+-- 8 movies --
+SELECT 
+	g.genre_name,
+	COUNT(*) as [Num_mujeres] 
+FROM movie m 
+inner join movie_genres mg 
+on mg.movie_id = m.movie_id 
+inner join genre g 
+on g.genre_id = mg.genre_id 
+inner join movie_cast mc 
+on mc.movie_id = m.movie_id 
+inner join gender g2 
+on g2.gender_id = mc.gender_id
+WHERE g.genre_name = 'Drama'
+GROUP BY g.genre_name HAVING COUNT(*) > 1 
+
+-- 9 movies --
+SELECT 
+	m.title,
+	l.language_name 
+FROM movie m 
+inner join movie_languages ml 
+on ml.movie_id = m.movie_id 
+inner join [language] l 
+on l.language_id = ml.language_id 
+order by m.title 
+
+-- 10 movies --
+SELECT TOP 8
+	k.keyword_name,
+	COUNT(*) as [Repetidos]
+FROM movie m 
+inner join movie_keywords mk 
+on mk.movie_id = m.movie_id 
+inner join keyword k 
+on k.keyword_id = mk.keyword_id 
+GROUP BY k.keyword_name HAVING COUNT(*) > 1
+ORDER BY [Repetidos] DESC 
